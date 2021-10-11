ac4c898c43a09cabcde4276cc50f1eb87265ab32
carolina higo <maracarol9@gmail.com>
2021-10-11 17:27:39 -0300

    feat(Header):  create component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,20 @@
-export default function Header() {
-  // TODO
+import Image from 'next/image';
+
+import styles from './header.module.scss';
+
+export default function Header(): JSX.Element {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <a href="/">
+          <Image
+            src="/images/Logo.svg"
+            alt="logo"
+            width={238.62}
+            height={25.63}
+          />
+        </a>
+      </div>
+    </header>
+  );
 }
