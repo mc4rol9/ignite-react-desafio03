b4dc56b510610282f8bca9fda4c99772d4b9de4c
carolina higo <maracarol9@gmail.com>
2021-10-11 22:00:14 -0300

    fix(Header):  add link to redirect

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 import styles from './header.module.scss';
 
@@ -6,14 +7,16 @@ export default function Header(): JSX.Element {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <a href="/">
-          <Image
-            src="/images/Logo.svg"
-            alt="logo"
-            width={238.62}
-            height={25.63}
-          />
-        </a>
+        <Link href="/">
+          <a>
+            <Image
+              src="/images/Logo.svg"
+              alt="logo"
+              width={238.62}
+              height={25.63}
+            />
+          </a>
+        </Link>
       </div>
     </header>
   );
